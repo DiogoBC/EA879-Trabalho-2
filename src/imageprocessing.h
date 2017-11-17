e816a9cfc24050505e27e6268c321b32ed8c05b4
Diogo <diogocheng@gmail.com>
2017-11-17 05:06:47 -0200

    Função brilhoProcesso funcionando

--- src/imageprocessing.h
+++ src/imageprocessing.h
@@ -12,6 +12,7 @@ imagem abrir_imagem(char *nome_do_arquivo);
 void salvar_imagem(char *nome_do_arquivo, imagem *I);
 void liberar_imagem(imagem *i);
 void vmax_imagem (imagem *I, float vmax[3]);
+void brilhoDireto (imagem *I, float fator);
 void brilhoProcesso (imagem *I, float fator);
 void multiplicaLinha (int i, imagem *I, float *r, float *g, float *b, float fator);
 void atualizaImagem (imagem *I, float *r, float *g, float *b);
